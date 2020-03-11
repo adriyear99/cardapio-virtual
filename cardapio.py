32a3c0e841f07fb066ac87452948f4c01155ad70
Adriano Tavares Andrade <adriano.tavares@dinfor.uniceub>
2020-03-10 21:59:02 -0300

    versão atualizada

--- cardapio.py
+++ cardapio.py
@@ -2,7 +2,11 @@ print("|||||CARDÁPIO VIRTUAL|||||")
 
 def cadastro:
     nome = input("Digite seu nome: ")
+    while len(nome) == 0:
+        nome = input("Digite seu nome: ")
+        
     email = input("Digite seu e-mail: ")
+    
     telefone = input("Digite seu telefone: ")
 
     print("Usuário cadastrado com sucesso!")
