2ad099d48a3f803a39bad6d58be854ca35673cff
Arthur de Aguiar <arthur.de.aguiar123@gmail.com>
2020-03-11 08:29:44 +0000

    Update cardapio.py

--- cardapio.py
+++ cardapio.py
@@ -1,13 +1,33 @@
+import register
+
 print("|||||CARDÁPIO VIRTUAL|||||")
 
-def cadastro:
-    nome = input("Digite seu nome: ")
-    while len(nome) == 0:
-        nome = input("Digite seu nome: ")
-        
-    email = input("Digite seu e-mail: ")
-    
-    telefone = input("Digite seu telefone: ")
+def Register():
+	while (True):
+		name = input("Digite seu nome: ")
+		email = input("Digite seu e-mail: ")
+		password = input("Digite sua senha: ")
+		cell_number = input("Digite seu telefone: ")
+		if (register.Register(email, password, name, cell_number)):
+			print("Registrado com sucesso!")
+			return
+		print("Falha no registro de conta. Tente novamente")
 
-    print("Usuário cadastrado com sucesso!")
-    
+def Login():
+	while (True):
+		email = input("Digite seu e-mail: ")
+		password = input("Digite sua senha: ")
+		if (register.Login(email, password)):
+			print("Logado com sucesso!")
+			return
+		print("Falha no login. Tente novamente")
+
+while (True):
+	choice = int(input("1 para registrar e 2 para logar: "))
+
+	if (choice == 1):
+		print("--------------Registro----------------")
+		Register()
+	if (choice == 2):
+		print("----------------Login-----------------")
+		Login()
