c03a6466c3014e5550277738018aa5d1c1bde70c
adriano.tavares.andrade <adriano.tavares.andrade@gmail.com>
2020-03-31 21:22:48 -0300

    versão atualizada

--- cardapio.py
+++ cardapio.py
@@ -1,13 +1,19 @@
-print("|||||CARDÁPIO VIRTUAL|||||")
-
-def cadastro:
+def cadastro():
     nome = input("Digite seu nome: ")
     while len(nome) == 0:
         nome = input("Digite seu nome: ")
-        
+
     email = input("Digite seu e-mail: ")
-    
+
     telefone = input("Digite seu telefone: ")
+    while len(telefone) < 8:
+        telefone = input("Digite seu telefone: ")
 
     print("Usuário cadastrado com sucesso!")
-    
+
+
+def main():
+    print("CARDÁPIO VIRTUAL")
+    cadastro()
+
+main()
