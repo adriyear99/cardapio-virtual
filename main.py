5690a87902ee3e45e07b8a692890b2e535199b4d
adriano.tavares.andrade <adriano.tavares.andrade@gmail.com>
2020-04-02 20:58:14 -0300

    versão atualizada

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class cardapio(BoxLayout):
+    pass
+
+class interface(App):
+    def build(self):
+        return cardapio()
+
+interface().run()
+    
