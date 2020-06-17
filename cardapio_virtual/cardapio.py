3577442661215fb85499036308f9a49f875dcfb4
adriano.tavares.andrade <adriano.tavares.andrade@gmail.com>
2020-06-17 19:20:22 -0300

    versão teste

--- cardapio_virtual/cardapio.py
+++ cardapio_virtual/cardapio.py
@@ -1,7 +1,7 @@
 import pygame, pygame.mixer
 import os
 import sys
-from .modelo import Usuario
+from cardapio_virtual.modelo.Usuario import Usuario
 
 pygame.init()
 pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
@@ -30,10 +30,19 @@ r2 = (240,200,300,100)
 r3 = (240,300,300,100)
 r4 = (240,400,300,100)
 enviar = (240,500,300,100)
+coca = (50,200,100,100)
+guarana = (100,200,100,100)
+fantauva = (150,200,100,100)
+fantalaranja = (200,200,100,100)
+comida1 = (50,50,100,100)
+comida2 = (100,50,100,100)
+comida3 = (150,50,100,100)
+comida4 = (200,50,100,100)
 
 #Som
 som = pygame.mixer.Sound('tracks/som.wav')
 clique = pygame.mixer.Sound('tracks/clique.wav')
+erro = pygame.mixer.Sound('tracks/erro.wav')
 
 #Caixas de Texto
 caixaNome = pygame.Rect(240,100,300,100)
@@ -43,9 +52,10 @@ caixaEmail = pygame.Rect(240,400,300,100)
 #Cores
 cor_inativa = pygame.Color('lightskyblue3')
 cor_ativa = pygame.Color('dodgerblue2')
-cor = cor_inativa
+corNome = cor_inativa
+corCpf = cor_inativa
+corEmail = cor_inativa
 blue = (0,191,255)
-
 
 
 def Exibir_Imagem(dados,nome):
@@ -71,16 +81,19 @@ def Exibir(cena):
         Exibir_Imagem(background, 'background_menu.jpg')
 
         screen.blit(pygame.font.SysFont("Arial bold",30).render("Nome",True,(255,255,255)),[250,70])
-        pygame.draw.rect(screen,cor,caixaNome,2)
-        screen.blit(pygame.font.SysFont("Arial bold",60).render(nome,True,cor),(caixaNome.x+7,caixaNome.y+10))
+        pygame.draw.rect(screen,corNome,caixaNome,2)
+        screen.blit(pygame.font.SysFont("Arial bold",60).render(nome,True,blue),(caixaNome.x+7,caixaNome.y+10))
+        screen.blit(pygame.font.SysFont("Arial bold", 30).render(user.getNome(), True, (255, 255, 255)), [250, 120])
 
         screen.blit(pygame.font.SysFont("Arial bold",30).render("CPF",True,(255,255,255)),[250,220])
-        pygame.draw.rect(screen,cor,caixaCpf,2)
-        screen.blit(pygame.font.SysFont("Arial bold",60).render(cpf,True,cor),(caixaCpf.x+7,caixaCpf.y+10))
+        pygame.draw.rect(screen,corCpf,caixaCpf,2)
+        screen.blit(pygame.font.SysFont("Arial bold",60).render(cpf,True,blue),(caixaCpf.x+7,caixaCpf.y+10))
+        screen.blit(pygame.font.SysFont("Arial bold", 30).render(user.getCpf(), True, (255, 255, 255)), [250, 270])
 
         screen.blit(pygame.font.SysFont("Arial bold",30).render("E-mail",True,(255,255,255)),[250,370])
-        pygame.draw.rect(screen,cor,caixaEmail,2)
-        screen.blit(pygame.font.SysFont("Arial bold",60).render(email,True,cor),(caixaEmail.x+7,caixaEmail.y+10))
+        pygame.draw.rect(screen,corEmail,caixaEmail,2)
+        screen.blit(pygame.font.SysFont("Arial bold",60).render(email,True,blue),(caixaEmail.x+7,caixaEmail.y+10))
+        screen.blit(pygame.font.SysFont("Arial bold", 30).render(user.getEmail(), True, (255, 255, 255)), [250, 420])
 
         if not valido:
             Exibir_Imagem(enviar,'enviarOFF.jpg')
@@ -94,6 +107,34 @@ def Exibir(cena):
         Exibir_Imagem(r3,'restaurante3.jpg')
         Exibir_Imagem(r4,'restaurante4.jpg')
 
+    elif cena == 3:
+        Exibir_Imagem(comida1,'baconOFF.jpg')
+        Exibir_Imagem(comida2, 'baconOFF.jpg')
+        Exibir_Imagem(comida3, 'baconOFF.jpg')
+        Exibir_Imagem(comida4, 'baconOFF.jpg')
+        Exibir_Imagem(coca, 'cocaOFF.jpg')
+        Exibir_Imagem(guarana, 'guaranaOFF.jpg')
+        Exibir_Imagem(fantauva, 'fantauvaOFF.jpg')
+        Exibir_Imagem(fantalaranja, 'fantalaranjaOFF.jpg')
+
+    elif cena == 4:
+        Exibir_Imagem(coca,'cocaOFF.jpg')
+        Exibir_Imagem(guarana,'guaranaOFF.jpg')
+        Exibir_Imagem(fantauva,'fantauvaOFF.jpg')
+        Exibir_Imagem(fantalaranja,'fantalaranjaOFF.jpg')
+
+    elif cena == 5:
+        Exibir_Imagem(coca,'cocaOFF.jpg')
+        Exibir_Imagem(guarana,'guaranaOFF.jpg')
+        Exibir_Imagem(fantauva,'fantauvaOFF.jpg')
+        Exibir_Imagem(fantalaranja,'fantalaranjaOFF.jpg')
+
+    elif cena == 6:
+        Exibir_Imagem(coca,'cocaOFF.jpg')
+        Exibir_Imagem(guarana,'guaranaOFF.jpg')
+        Exibir_Imagem(fantauva,'fantauvaOFF.jpg')
+        Exibir_Imagem(fantalaranja,'fantalaranjaOFF.jpg')
+
 
 def Acoes(cena):
     global nome
@@ -104,7 +145,9 @@ def Acoes(cena):
     global emailAtivo
     global valido
     global running
-    global cor
+    global corNome
+    global corCpf
+    global corEmail
     
     for event in pygame.event.get():
 
@@ -113,22 +156,35 @@ def Acoes(cena):
 
         elif event.type == pygame.KEYDOWN:
             if nomeAtivo:
-                if event.key == pygame.K_BACKSPACE or len(user.nome) > 50:
+                if event.key == pygame.K_BACKSPACE or len(user.getNome()) > 50:
                     user.backspaceNome()
+                elif event.key == 13:
+                    nomeAtivo = False
+                    corNome = cor_inativa
+                    cpfAtivo = True
+                    corCpf = cor_ativa
+                    return 1
                 else:
-                    user.nome += event.unicode
+                    user.appendNome(event.unicode)
 
             if cpfAtivo:
-                if event.key == pygame.K_BACKSPACE or len(user.cpf) > 11:
+                if event.key == pygame.K_BACKSPACE or len(user.getCpf()) > 12:
                     user.backspaceCpf()
+                elif event.key == 13:
+                    cpfAtivo = False
+                    corCpf = cor_inativa
+                    emailAtivo = True
+                    corEmail = cor_ativa
+                    return 1
                 else:
-                    user.cpf += event.unicode
+                    user.appendCpf(event.unicode)
 
             if emailAtivo:
-                if event.key == pygame.K_BACKSPACE or len(user.nome) > 50:
+                if event.key == pygame.K_BACKSPACE or len(user.getEmail()) > 50:
                     user.backspaceEmail()
                 else:
-                    user.email += event.unicode
+                    if event.key != 13:
+                        user.appendEmail(event.unicode)
         
         elif event.type == pygame.MOUSEBUTTONDOWN:
 
@@ -149,62 +205,80 @@ def Acoes(cena):
             elif cena == 1:
 
                 if caixaNome.collidepoint(event.pos):
-                    nomeAtivo = not nomeAtivo
+                    nomeAtivo = True
+                    corNome = cor_ativa
+                    corCpf = cor_inativa
+                    corEmail = cor_inativa
                     
                 elif caixaCpf.collidepoint(event.pos):
-                    cpfAtivo = not cpfAtivo
+                    cpfAtivo = True
+                    corCpf = cor_ativa
+                    corNome = cor_inativa
+                    corEmail = cor_inativa
                     
                 elif caixaEmail.collidepoint(event.pos):
-                    emailAtivo = not emailAtivo
+                    emailAtivo = True
+                    corEmail = cor_ativa
+                    corNome = cor_inativa
+                    corCpf = cor_inativa
 
                 elif pygame.Rect(enviar).collidepoint(event.pos):
-                    if len(user.nome) > 0 and len(user.cpf) == 11 and len(user.email) > 10:
+                    if len(user.getNome()) > 0 and len(user.getCpf()) == 11 and len(user.getEmail()) > 10:
                         log("Dados de usuário registrados")
                         clique.play()
                         cena = 2
                         return cena
-                    
+                    else:
+                        log("É preciso preencher todos os campos para prosseguir")
+                        erro.play()
+
                 else:
                     nomeAtivo = False
                     cpfAtivo = False
                     emailAtivo = False
-                    
-
-                
+                    corNome = cor_inativa
+                    corCpf = cor_inativa
+                    corEmail = cor_inativa
 
             elif cena == 2:
 
                 if pygame.Rect(r1).collidepoint(event.pos):
                     log("Restaurante 1 selecionado")
                     clique.play()
-                    cena = 2
+                    cena = 3
                     return cena
 
                 elif pygame.Rect(r1).collidepoint(event.pos):
                     log("Restaurante 2 selecionado")
                     clique.play()
-                    cena = 3
+                    cena = 4
                     return cena
 
                 elif pygame.Rect(r1).collidepoint(event.pos):
                     log("Restaurante 3 selecionado")
                     clique.play()
-                    cena = 4
+                    cena = 5
                     return cena
 
                 elif pygame.Rect(r1).collidepoint(event.pos):
                     log("Restaurante 4 selecionado")
                     clique.play()
-                    cena = 5
+                    cena = 6
                     return cena
 
+            elif cena == 3:
+                break
+
+            elif cena == 4:
+                break
+
+            elif cena == 5:
+                break
+
+            elif cena == 6:
+                break
                 
     return cena
-
-                
-
-
-
 
 
 screen = pygame.display.set_mode((800,600))
